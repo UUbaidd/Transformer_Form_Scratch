3dba7bc90950bb7598a93cac893b01a453313ddf
Muahmmad Ubaid <152315066+UUbaidd@users.noreply.github.com>
2026-05-07 01:54:07 -0700

    Create Positional_embeddings.py

---
+++ Positional_embeddings.py
@@ -0,0 +1,10 @@
+import numpy as np
+def get_positional_encoding(seq_len, d_model):
+    pe = np.zeros((seq_len, d_model))
+    for pos in range(seq_len):
+        for i in range(0, d_model, 2):
+            pe[pos, i] = np.sin(pos / (10000 ** (i / d_model)))
+            if i + 1 < d_model:
+                pe[pos, i + 1] = np.cos(pos / (10000 ** (i / d_model)))
+    return pe
+pe = get_positional_encoding(seq_len, D_MODEL)
